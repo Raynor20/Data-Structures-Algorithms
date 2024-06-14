61cbb0b4ae84afb89a5bab1309db4dd2fb5f62c0
Raynor20 <90785676+Raynor20@users.noreply.github.com>
2024-06-14 14:36:59 +0800

    Update StackArray.c

--- STACK/StackArray.c
+++ STACK/StackArray.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
 #include "StackArray.h"
 
-void isEmpty(StackArrayList s) {
+bool isEmpty(StackArrayList *s) {
 	return s->top == -1;
 }
 
-void isFull(StackArrayList s) {
+bool isFull(StackArrayList *s) {
 	return s->top == MAX - 1;
 }
 
@@ -15,62 +17,81 @@ void initStack(StackArrayList *s) {
 	s->top = -1;
 }
 StackArrayList createStack() {
-	StackArrayList stack;
-	initStack(&stack);
-	return stack;
+	StackArrayList s;
+	initStack(&s);
+	return s;
 }
 
-bool stack_push(StackArrayList *s, int elem) {
+bool push(StackArrayList *s, int elem) {
 		if(isFull(s)) {
 			printf("Stack is Full!\n");
+			return false;
 		}
-		return false;
-	}
-	s->data[++(s->top)] = elem;
+	s->data[++s->top] = elem;
 	return true;
 }
-bool stack_pop(StackArrayList *s) {
+bool pop(StackArrayList *s) {
 	if(isEmpty(s)) {
 		printf("Stack is Empty!\n");
+		return false;
 	}
 	s->top--;
+	return true;
 }
-int stack_peek(StackArrayList s) {
-	if(isEmpty(s)) {
+int peek(StackArrayList s) {
+	if(isEmpty(&s)) {
 		printf("Stack is Empty!\n");
+		return -1;
 	}
-	return s->data[s->top];
+	return s.data[s.top];
 }
 
 void display(StackArrayList s) {
-	StackArrayList newStack;
-	initStack(&newStack);
+	StackArrayList newStack = createStack();
 	
-	while (!isEmpty(s)) {
+	while (!isEmpty(&s)) {
 		push(&newStack, peek(s));
-		pop(s);
+		pop(&s);
 	}
 	while (!isEmpty(&newStack)) {
-		printf("%d", peek(&newStack));
+		printf("%4d", peek(newStack));
 		pop(&newStack);
 	}
+	printf("\n");
 }
 void visualize(StackArrayList s) {
-	if (isEmpty(s)) {
+	int i;
+	if (isEmpty(&s)) {
 		printf("Stack is Empty!\n");
-		return;
 	}
-	
+	printf("%s %7s %7s\n", "INDEX", "DATA", "TOP");
+	for(i = s.top; i >= 0; i--) {
+		printf("%3d %8d ", i, s.data[i]);
+		if (s.data[i] == s.data[s.top]) {
+			printf("%8s ", "<-- top");
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
+StackArrayList getEven(StackArrayList *s) {
+	StackArrayList newStack = createStack();
+	StackArrayList tempStack = createStack();
+	
+	while(!isEmpty(s)) {
+		int elem = peek(*s);
+		pop(s);
+	if (elem % 2 == 0) {
+            push(&newStack, elem);
+        } else {
+            push(&tempStack, elem);
+        }
+    }
 
-int main() {
-	StackArrayList s;
-	initStack(&s);
-	
-	push(&s, 10);
-	push(&s, 20);
-	push(&s, 30);
-	visualize(&s);
-	
-	return 0;
+    while (!isEmpty(&tempStack)) {
+        push(s, peek(tempStack));
+        pop(&tempStack);
+    }
+
+    return newStack;
 }
